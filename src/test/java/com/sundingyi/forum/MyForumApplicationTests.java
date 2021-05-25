02d3fd47705c1b6cffac28aa117aacbcb8891284
42Sun <i@sundingyi.com>
2021-05-25 15:01:18 +0800

    first commit

---
+++ src/test/java/com/sundingyi/forum/MyForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sundingyi.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyForumApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
