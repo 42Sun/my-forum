22e08a6e5a40677b7dad12fd506cd94b76eedf4b
42Sun <i@sundingyi.com>
2021-05-26 19:59:53 +0800

    session

--- src/main/java/com/sundingyi/forum/controller/OAController.java
+++ src/main/java/com/sundingyi/forum/controller/OAController.java
@@ -8,11 +8,11 @@ import com.sundingyi.forum.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -32,7 +32,7 @@ public class OAController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest httpServletRequest) {
+                           HttpServletResponse httpServletResponse) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setClient_id(clientId);
@@ -42,13 +42,15 @@ public class OAController {
         GithubUser githubUser = githubProvider.getGithubUser(accessToken);
         if (githubUser != null) { // 登录成功，写入 cookies 和 session
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
-            httpServletRequest.getSession().setAttribute("githubUser", githubUser);
+            
+            httpServletResponse.addCookie(new Cookie("token", token));
             return "redirect:/";
         } else { // 登录失败，重新登录
             return "redirect:/";
--- src/main/java/com/sundingyi/forum/controller/indexController.java
+++ src/main/java/com/sundingyi/forum/controller/indexController.java
@@ -1,12 +1,34 @@
 package com.sundingyi.forum.controller;
 
+import com.sundingyi.forum.mapper.UserMapper;
+import com.sundingyi.forum.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class indexController {
+    @Autowired
+    private UserMapper userMapper;
+    
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest httpServletRequest) {
+        Cookie[] cookies = httpServletRequest.getCookies();
+        
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if (user != null) {
+                    httpServletRequest.getSession().setAttribute("githubUser", user);
+                }
+                break;
+            }
+        }
+        
         return "index";
     }
 }
