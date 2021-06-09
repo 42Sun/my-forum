25389c1f983d9e2e35821ca47b9a1404c9e00f80
42Sun <i@sundingyi.com>
2021-06-09 16:49:40 +0800

    refactor github user name

--- src/main/java/com/sundingyi/forum/controller/OAController.java
+++ src/main/java/com/sundingyi/forum/controller/OAController.java
@@ -47,7 +47,7 @@ public class OAController {
             User user = new User();
             String token = UUID.randomUUID().toString();
             user.setToken(token);
-            user.setName(githubUser.getName());
+            user.setName(githubUser.getLogin());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setAvatarUrl(githubUser.getAvatarUrl());
             userService.createOrUpdate(user);
